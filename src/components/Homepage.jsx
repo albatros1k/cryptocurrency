c0b148bbd2f5c453719ddae2cd2a18dd8212774d
Albert <superalbertem@gmail.com>
2021-09-26 19:15:33 +0300

    Created new Api files

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,9 +3,20 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from ".";
+
 const { Title } = Typography;
 
 function Homepage() {
+  const { data, isFetching } = useGetCryptosQuery(10);
+
+  const globalStats = data?.data?.stats;
+
+  console.log(data);
+
+  if (isFetching) return "Loading ... ";
+
   return (
     <>
       <Title level={2} className="heading">
@@ -13,22 +24,51 @@ function Homepage() {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total exchanges" value="5" />
+          <Statistic
+            title="Total exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total markets" value="5" />
+          <Statistic
+            title="Total markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
-        
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
+
+import { useGetCryptoNewsQuery } from "../services/cryotoNewsApi";
 
 const News = () => {
   return <div>news</div>;
